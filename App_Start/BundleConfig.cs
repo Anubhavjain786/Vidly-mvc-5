4d94d202272e6d23c40dda8c352e61a2b6116b80
Anubhav Jain <anubhavj660@gmail.com>
2019-09-15 20:19:53 +0530

    Add a bootstrap theme.

--- App_Start/BundleConfig.cs
+++ App_Start/BundleConfig.cs
@@ -23,7 +23,7 @@ namespace Vidly
                       "~/Scripts/bootstrap.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
+                      "~/Content/bootstrap-lumen.css",
                       "~/Content/site.css"));
         }
     }
