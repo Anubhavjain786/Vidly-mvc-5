8acfeb4001651bcefaee8c51b154d65ae52b1a1d
Anubhav Jain <anubhavj660@gmail.com>
2019-09-16 00:39:33 +0530

    Display the list of customers and movies

---
+++ Controllers/CustomersController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customers = GetCustomers();
+            return View(customers);
+        }
+        public ActionResult Details(int id)
+        {
+            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+        private IEnumerable<Customer> GetCustomers()
+        {
+            return new List<Customer>
+            { new Customer { Id = 1, Name = "John Smith" },
+                new Customer { Id = 2, Name = "Mary Williams" }
+            };
+        }
+    }
+}
--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -11,9 +11,19 @@ namespace Vidly.Controllers
     public class MoviesController : Controller
     {
         // GET: Movies
-        public ActionResult Index()
+        public ViewResult Index()
         {
-            return View();
+            var movies = GetMovies();
+
+            return View(movies);
+        }
+        private IEnumerable<Movie> GetMovies()
+        {
+            return new List<Movie>
+            {
+                new Movie { Id = 1, Name = "Shrek" },
+                new Movie { Id = 2, Name = "Wall-e" }
+            };
         }
         // GET: Movies/Random
         public ActionResult Random()
