50c8c54bf5d86745fd965ea6f6c62beef3d83530
Anubhav Jain <anubhavj660@gmail.com>
2019-09-15 20:07:56 +0530

    MVC in Action.

---
+++ Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+
+namespace Vidly.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+
+            return View(movie);
+        }
+    }
+}
