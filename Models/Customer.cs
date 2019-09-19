b70d13f036876ba180c29a32fb06e546f9f25993
Anubhav Jain <anubhavj660@gmail.com>
2019-09-19 17:58:04 +0530

    Apply Business Logic Age Validation

--- Models/Customer.cs
+++ Models/Customer.cs
@@ -22,6 +22,7 @@ namespace Vidly.Models
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsIfAMember]
         public DateTime? Birthdate { get; set; }
 
     }
