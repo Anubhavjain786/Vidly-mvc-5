dd200060184a7fb45d20afa52c72a463d5baec5a
Anubhav Jain <anubhavj660@gmail.com>
2019-09-19 18:01:36 +0530

    Form Cover With Anti Forgery Token (Anti CSRF)

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -36,6 +36,7 @@ namespace Vidly.Controllers
         }
 
         [HttpPost]
+        [ValidateAntiForgeryToken]
         public ActionResult Save(Customer customer)
         {
             if (!ModelState.IsValid)
