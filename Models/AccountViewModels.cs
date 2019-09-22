bbf3e220dba78a5290bf5dfe58fcf50099b5e897
Anubhav Jain <anubhavj660@gmail.com>
2019-09-23 01:36:23 +0530

    Add Aadhar Card to user.

--- Models/AccountViewModels.cs
+++ Models/AccountViewModels.cs
@@ -65,6 +65,9 @@ namespace Vidly.Models
     public class RegisterViewModel
     {
         [Required]
+        [Display(Name ="Aadhar Card")]
+        public string AadharCard { get; set; }
+        [Required]
         [EmailAddress]
         [Display(Name = "Email")]
         public string Email { get; set; }
